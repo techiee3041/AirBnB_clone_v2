7a7ddb55df04773941033b46030070f5d9fd1bf5
dancan-agweli <agwelidanan32@gmail.com>
2023-08-23 04:52:46 -0700

    airbnb mysql

--- models/user.py
+++ models/user.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
 
+import os
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """User class with various attributes."""
+    __tablename__ = 'users'
+    
+    # Define attributes based on storage type
+    email = Column(String(128), nullable=False) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    password = Column(String(128), nullable=False) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    first_name = Column(String(128), nullable=True) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    last_name = Column(String(128), nullable=True) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    
+    # Define relationships based on storage type
+    places = relationship('Place', cascade="all, delete, delete-orphan", backref='user') if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+    reviews = relationship('Review', cascade="all, delete, delete-orphan", backref='user') if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+
