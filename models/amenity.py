e31da38aa8928a4882d0014c11dfed6b696747b9
dancan-agweli <agwelidanan32@gmail.com>
2023-08-23 04:18:35 -0700

    airbnb clone-mysql

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
 
+import os
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Represents an amenity data set. Defines 'name' property based on storage type."""
+    __tablename__ = 'amenities'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+    else:
+        name = ''
+
