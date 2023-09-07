443c9e52f658208b6d8894bdaf257dd41307890c
dancan-agweli <agwelidanan32@gmail.com>
2023-09-07 04:38:45 -0700

    updates

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,60 +1,61 @@
 #!/usr/bin/python3
-""" web application deployment with Fabric."""
-import os
+"""Fabric script to distribute an archive to web servers"""
+
+from fabric.api import env, put, run, hosts
+from os.path import exists
 from datetime import datetime
-from fabric.api import env, local, put, run, runs_once
 
-# Define variables
-env.hosts = ["100.25.200.118", "54.237.88.4"]
-archive_destination = "versions"
-archive_name_format = "web_static_{year}{month}{day}{hour}{minute}{second}.tgz"
+# Define server information
+servers = {
+    '253642-web-01': {
+        'ip': '100.25.200.118',
+        'user': 'ubuntu'
+    },
+    '253642-web-02': {
+        'ip': '54.237.88.4',
+        'user': 'ubuntu'
+    }
+}
 
-@runs_once
+# Set the environment variables based on server information
+env.hosts = [servers[server]['ip'] for server in servers]
+env.user = servers['253642-web-01']['user']  # You can choose any server as a reference
+
 def do_pack():
-    """Archives the static files."""
-    if not os.path.isdir(archive_destination):
-        os.mkdir(archive_destination)
-    cur_time = datetime.now()
-    archive_name = archive_name_format.format(
-        year=cur_time.year,
-        month=cur_time.month,
-        day=cur_time.day,
-        hour=cur_time.hour,
-        minute=cur_time.minute,
-        second=cur_time.second
-    )
-    archive_path = os.path.join(archive_destination, archive_name)
+    """Create a compressed archive of your web_static directory"""
     try:
-        print("Packing web_static to {}".format(archive_path))
-        local("tar -cvzf {} web_static".format(archive_path))
-        archive_size = os.stat(archive_path).st_size
-        print("web_static packed: {} -> {} Bytes".format(archive_path, archive_size))
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static".format(timestamp))
+        return "versions/web_static_{}.tgz".format(timestamp)
     except Exception:
-        archive_path = None
-    return archive_path
+        return None
 
 def do_deploy(archive_path):
-    """Deploys the static files to the host servers.
-        archive_path (str): The path to the archived static files.
-    """
-    if not os.path.exists(archive_path):
+    """Distribute an archive to the web servers"""
+    if not exists(archive_path):
         return False
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
+
     try:
-        put(archive_path, "/tmp/{}".format(file_name))
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        run("rm -rf /tmp/{}".format(file_name))
-        run("mv {}web_static/* {}".format(folder_path, folder_path))
-        run("rm -rf {}web_static".format(folder_path))
+        archive_name = archive_path.split('/')[-1]
+        path_no_extension = "/data/web_static/releases/{}".format(archive_name.split('.')[0])
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(path_no_extension))
+        run("tar -xzf /tmp/{} -C {}".format(archive_name, path_no_extension))
+        run("rm /tmp/{}".format(archive_name))
+        run("mv {}/web_static/* {}/".format(path_no_extension, path_no_extension))
+        run("rm -rf {}/web_static".format(path_no_extension))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_path))
-        print('New version deployed!')
-        success = True
+        run("ln -s {} /data/web_static/current".format(path_no_extension))
+        return True
     except Exception:
-        success = False
-    return success
+        return False
+
+def deploy():
+    """Deploy the web_static content to the web servers"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+
+    return do_deploy(archive_path)
 
