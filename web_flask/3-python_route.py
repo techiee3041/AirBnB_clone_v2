beda3e94a52c1bc4f10634f095d02dd9c79973cb
techiee3041 <doreenoms6699@gmail.com>
2023-09-24 16:15:27 +0300

    added a python route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,6 +25,7 @@ def cisfun(text):
     return 'C ' + text.replace('_', ' ')
 
 
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pcool(text="is cool"):
     """display “Python ”, followed by the value of the text variable"""
